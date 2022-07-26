04b74ffadbd0b2e7da27744c50517c044cc1a7a2
Sofia Franco <sofiafrancor@gmail.com>
2022-07-26 14:33:33 -0500

    first commit

--- empresas/views.py
+++ empresas/views.py
@@ -11,36 +11,6 @@ from .models import Empresa
 from .serializers import EmpresaSerializer
 # Create your views here.
 
-@api_view
-def EmpresaApi(request,id=0):
-    if request.method=='GET':
-        empresa = Empresa.objects.all()
-        serializer = EmpresaSerializer(empresa, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    elif request.method=='POST':
-        empresa=JSONParser().parse(request)
-        serializer = EmpresaSerializer(data=empresa)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse("Added Successfully!!" , safe=False)
-        return JsonResponse("Failed to Add.",safe=False)
-    
-    elif request.method=='PUT':
-        empresa = JSONParser().parse(request)
-        department=Empresa.objects.get(DepartmentId=empresa['id'])
-        serializer=EmpresaSerializer(department,data=empresa)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse("Updated Successfully!!", safe=False)
-        return JsonResponse("Failed to Update.", safe=False)
-
-    elif request.method=='DELETE':
-        department=Empresa.objects.get(DepartmentId=id)
-        department.delete()
-        return JsonResponse("Deleted Succeffully!!", safe=False)
-
-
 @api_view(['POST'])
 def registrar(request):
 	serializer = EmpresaSerializer(data = request.data)
